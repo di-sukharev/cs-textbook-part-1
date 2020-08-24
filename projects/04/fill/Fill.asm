b7552906598ab7dafcc0f7ddcf55c3e5d05ef131
Dima <dima@sukharev.io>
2020-08-24 10:56:17 +0300

    cleared 04 project

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +6,4 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// реализация
