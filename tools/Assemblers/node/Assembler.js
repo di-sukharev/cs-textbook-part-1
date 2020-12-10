7bd9927ed68f65a9eb14960730212b3644c6b862
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2020-12-10 11:03:42 +0300

    100% ready assembler, needs refactoring

--- tools/Assemblers/node/Assembler.js
+++ tools/Assemblers/node/Assembler.js
@@ -7,7 +7,9 @@ const INSTRUCTIONS = {
 };
 
 class Assembler {
+    freeVariableAddress = 16;
     variablesTable = {
+        // pre-defined vars
         R0: 0,
         R1: 1,
         R2: 2,
@@ -24,6 +26,10 @@ class Assembler {
         R13: 13,
         R14: 14,
         R15: 15,
+        SCREEN: 16384,
+        KBD: 24578,
+        SP: 0,
+        // TODO: add VM vars
     };
 
     labelsTable = {};
@@ -38,9 +44,9 @@ class Assembler {
 
         const file = fs.readFileSync(inputFileName, "utf8");
 
-        const hack = this.parse(file);
+        const assembled = this.parse(file);
 
-        fs.writeFile(`${outputFileName}.hack`, hack, () => {});
+        fs.writeFile(`${outputFileName}.hack`, assembled, () => {});
     }
 
     parse(file) {
@@ -55,21 +61,41 @@ class Assembler {
             .split("\r\n")
             .map(removeComments)
             .filter(removeWhitespaces)
-            .filter(this.createLablesTable.bind(this)) // todo: remove bind(this)
+            .filter(this.initLabelsAndVariables.bind(this)) // todo: remove bind(this)
             .map(this.translate.bind(this)) // todo: remove bind(this)
             .join("\n");
 
         return instructions;
     }
 
-    createLablesTable(instruction, lineNumber) {
-        if (!instruction.includes("(")) return true;
+    // todo: refactor, and change this filter() to smth else
+    initLabelsAndVariables(instruction, lineNumber) {
+        // variable
+        if (instruction.includes("@")) {
+            let value = instruction.slice(1);
+            // pre-defined
+            if (this.variablesTable[value] !== undefined) return true;
 
-        const [isLabel, label] = instruction.match(/\((.*)\)/i);
+            // lowercase is a variable, need to init its value in the variablesTable
+            if (value.match(/[a-z]+/)) {
+                this.variablesTable[value] = this.freeVariableAddress;
+                this.freeVariableAddress++;
+            }
 
-        if (isLabel) this.labelsTable[label] = lineNumber + 1;
+            // all variables will be changed later
+            return true;
+        }
 
-        return false;
+        // its a (LABEL)
+        if (instruction.includes("(")) {
+            const [isLabel, label] = instruction.match(/\((.*)\)/i);
+
+            if (isLabel) this.labelsTable[label] = lineNumber + 1;
+
+            return false;
+        }
+
+        return true;
     }
 
     translate(instruction) {
@@ -78,38 +104,39 @@ class Assembler {
                 return this.translateA(instruction);
             case INSTRUCTIONS.C:
                 return this.translateC(instruction);
-            case INSTRUCTIONS.L:
-                return this.translateL(instruction);
         }
     }
 
     getType(instruction) {
         if (instruction.includes("@")) return INSTRUCTIONS.A;
-        else if (instruction.includes("(")) return INSTRUCTIONS.L;
         else return INSTRUCTIONS.C;
     }
 
     translateA(instruction) {
-        const decToBin = (dec) => {
-            const bin = (+dec).toString(2);
-            return "000000000000000".slice(bin.length) + bin;
-        };
+        const decToBin = (dec) => (+dec).toString(2);
+        const fillWith15Bits = (bin) =>
+            "000000000000000".slice(bin.length) + bin;
 
         let value = instruction.slice(1);
 
-        if (typeof value === "string")
+        // it's a variable or label
+        if (value.match(/\D+/i))
             value = this.labelsTable[value] || this.variablesTable[value];
 
-        const hack = `0${decToBin(value)}`;
+        const hack = `0${fillWith15Bits(decToBin(value))}`;
 
-        return;
+        return hack;
     }
 
     translateC(instruction) {
         const [dstcmp, jmp] = instruction.split(";");
-        const [dst, cmp] = dstcmp.split("=");
+        const [dst, cmp] = dstcmp.includes("=")
+            ? dstcmp.split("=")
+            : [undefined, dstcmp];
 
-        const hack = `111${this.comp(cmp)}${this.dest(dst)}${this.jump(jmp)}`;
+        console.log({ instruction, dst, cmp, jmp });
+
+        const hack = `111${this.dest(dst)}${this.comp(cmp)}${this.jump(jmp)}`;
 
         return hack;
     }
@@ -117,15 +144,15 @@ class Assembler {
     dest(symbols) {
         // prettier-ignore
         switch (symbols) {
-            case "M":       return '001';
-            case "D":       return '010';
-            case "MD":      return '011';
-            case "A":       return '100';
-            case "AM":      return '101';
-            case "AD":      return '110';
-            case "AMD":     return '111';
-            case "":        return '000';
-            case undefined: return '000';
+            case "M":   return '001';
+            case "D":   return '010';
+            case "MD":  return '011';
+            case "A":   return '100';
+            case "AM":  return '101';
+            case "AD":  return '110';
+            case "AMD": return '111';
+            case "":    return '000';
+            default:    return '000';
         }
     }
 
@@ -133,35 +160,35 @@ class Assembler {
         // prettier-ignore
         switch (symbols) {
             // a = 0
-            case "0":       return '0101010';
-            case "1":       return '0111111';
-            case "-1":      return '0111010';
-            case "D":       return '0001100';
-            case "A":       return '0110000';
-            case "!D":      return '0001101';
-            case "!A":      return '0110001';
-            case "-D":      return '0001111';
-            case "-A":      return '0110011';
-            case "D+1":     return '0011111';
-            case "A+1":     return '0110111';
-            case "D-1":     return '0001110';
-            case "A-1":     return '0110010';
-            case "D+A":     return '0000010';
-            case "D-A":     return '0010011';
-            case "A-D":     return '0000111';
-            case "D&A":     return '0000000';
-            case "D|A":     return '0010101';
+            case "0":   return '0101010';
+            case "1":   return '0111111';
+            case "-1":  return '0111010';
+            case "D":   return '0001100';
+            case "A":   return '0110000';
+            case "!D":  return '0001101';
+            case "!A":  return '0110001';
+            case "-D":  return '0001111';
+            case "-A":  return '0110011';
+            case "D+1": return '0011111';
+            case "A+1": return '0110111';
+            case "D-1": return '0001110';
+            case "A-1": return '0110010';
+            case "D+A": return '0000010';
+            case "D-A": return '0010011';
+            case "A-D": return '0000111';
+            case "D&A": return '0000000';
+            case "D|A": return '0010101';
             // a = 1
-            case "M":       return '1110000';
-            case "!M":      return '1110001';
-            case "-M":      return '1110011';
-            case "M+1":     return '1110111';
-            case "M-1":     return '1110010';
-            case "D-M":     return '1000010';
-            case "M-D":     return '1000111';
-            case "D&M":     return '1000000';
-            case "D|M":     return '1010101';
-            case undefined: return '';
+            case "M":   return '1110000';
+            case "!M":  return '1110001';
+            case "-M":  return '1110011';
+            case "M+1": return '1110111';
+            case "M-1": return '1110010';
+            case "D-M": return '1000010';
+            case "M-D": return '1000111';
+            case "D&M": return '1000000';
+            case "D|M": return '1010101';
+            default:    return '';
         }
     }
 
@@ -176,7 +203,7 @@ class Assembler {
             case "JLE":     return '110';
             case "JMP":     return '111';
             case "":        return '000';
-            case undefined: return '000';
+            default:        return '000';
         }
     }
 }
