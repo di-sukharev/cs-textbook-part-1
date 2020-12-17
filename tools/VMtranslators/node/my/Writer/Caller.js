9b90f881d9e53c052f2fbb21ad03c9051e8c1651
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2020-12-17 20:57:38 +0300

    implementing _translateCall 50%

--- tools/VMtranslators/node/my/Writer/Caller.js
+++ tools/VMtranslators/node/my/Writer/Caller.js
@@ -1,3 +1,5 @@
+const { breakLines } = require("../tools");
+
 // todo: maybe CALLER_OPS_TYPE rename? and rename other contants?
 const CALLER_OPS = {
     call: "call",
@@ -23,9 +25,32 @@ class Caller {
         }
     }
 
-    _translateCall(name, args) {}
+    _translateCall(name, args) {
+        // LCL = SP; fix SP being off by one
+        const fixSP = () => breakLines`@SP MD=M+1 @LCL M=D`;
 
-    _translateFunction(name, localVars) {}
+        // arg_shift = num_args + 4, num_args=1
+        const shiftArg = () => breakLines`@${args + 4} D=A @SP D=M-D @ARG M=D`;
+
+        // save segments on stack
+        const pushSegments = () =>
+            ["LCL", "ARG", "THIS", "THAT"].reduce(
+                (res, seg) => (res += breakLines`@${seg} D=A @SP A=M M=D`)
+            );
+
+        const pushRIP = () =>
+            breakLines`@Main.fibonacci$genlabel$4 D=A @SP A=M M=D`;
+
+        // goto callee, set up a return label
+        const gotoCallee = () =>
+            breakLines`@Main.fibonacci 0;JMP (Main.fibonacci$genlabel$4)`;
+
+        return breakLines`${pushRIP()} ${pushSegments()} ${shiftArg()} ${fixSP()} ${gotoCallee()}`;
+    }
+
+    _translateFunction(name, localVars) {
+        return `(${name})`;
+    }
 
     _translateReturn(value) {}
 }
