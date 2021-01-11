4ea3af35e1dcf5e1813bac093be838ceafec00f9
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-01-11 22:37:16 +0300

    added .example. files to 07 and 08

--- projects/08/ProgramFlow/BasicLoop/BasicLoop.asm
+++ projects/08/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -1,85 +1,22 @@
-
+// INIT @SP | write_init
 @256
 D=A
 @SP
 M=D
-
-@RETURN_LABEL0
-D=A
-@SP
-A=M
-M=D
-
-@SP
-M=M+1
-
-@LCL
-D=M
-@SP
-A=M
-M=D
-
-@SP
-M=M+1
-
-@ARG
-D=M
-@SP
-A=M
-M=D
-
-@SP
-M=M+1
-
-@THIS
-D=M
-@SP
-A=M
-M=D
-
-@SP
-M=M+1
-
-@THAT
-D=M
-@SP
-A=M
-M=D
-
-@SP
-M=M+1
-
-@SP
-D=M
-@5
-D=D-A
-@0
-D=D-A
-@ARG
-M=D
-
-@SP
-D=M
-@LCL
-M=D
-
-@Sys.init
-0;JMP
-
-(RETURN_LABEL0)
-
+// push constant 0 | write_push
 @0
 D=A
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
+// pop local 0 | write_pop
 @LCL
 D=M
 @0
 D=D+A
+// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -88,41 +25,41 @@ D=M
 @R13
 A=M
 M=D
-
-(LOOP_START)
-
+// write_label
+(BasicLoop.$nofunction$LOOP_START)
+// push argument 0 | write_push
 @ARG
 D=M
 @0
 A=D+A
 D=M
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
+// push local 0 | write_push
 @LCL
 D=M
 @0
 A=D+A
 D=M
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
 @SP
 AM=M-1
 D=M
 A=A-1
-M=M+D
-
+M=D+M
+// pop local 0 | write_pop
 @LCL
 D=M
 @0
 D=D+A
+// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -131,36 +68,36 @@ D=M
 @R13
 A=M
 M=D
-
+// push argument 0 | write_push
 @ARG
 D=M
 @0
 A=D+A
 D=M
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
+// push constant 1 | write_push
 @1
 D=A
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
 @SP
 AM=M-1
 D=M
 A=A-1
 M=M-D
-
+// pop argument 0 | write_pop
 @ARG
 D=M
 @0
 D=D+A
+// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -169,31 +106,35 @@ D=M
 @R13
 A=M
 M=D
-
+// push argument 0 | write_push
 @ARG
 D=M
 @0
 A=D+A
 D=M
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
-
+A=M-1
+M=D
+// if-goto BasicLoop.$nofunction$LOOP_START | write_if
 @SP
 AM=M-1
 D=M
-@LOOP_START
-D;JGT
-
+@BasicLoop.$nofunction$LOOP_START
+D;JNE
+// push local 0 | write_push
 @LCL
 D=M
 @0
 A=D+A
 D=M
-@SP
-A=M
-M=D
+// push D on stack
 @SP
 M=M+1
+A=M-1
+M=D
+// write_endless_loop
+(BasicLoop.$nofunction$genlabel$1)
+@BasicLoop.$nofunction$genlabel$1
+0;JMP
