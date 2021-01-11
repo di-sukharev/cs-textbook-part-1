f785d05cd696ddd2086981c7c49a75352d7bbf8e
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-01-11 22:38:59 +0300

    added comments to tests

---
+++ projects/07/MemoryAccess/BasicTest/BasicTest.example.asm
@@ -0,0 +1,250 @@
+// push constant 10
+@10
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// pop local 0
+@LCL
+D=M
+@0
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push constant 21
+@21
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push constant 22
+@22
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// pop argument 2
+@ARG
+D=M
+@2
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// pop argument 1
+@ARG
+D=M
+@1
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push constant 36
+@36
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// pop this 6
+@THIS
+D=M
+@6
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push constant 42
+@42
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push constant 45
+@45
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// pop that 5
+@THAT
+D=M
+@5
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// pop that 2
+@THAT
+D=M
+@2
+D=D+A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push constant 510
+@510
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+// pop temp 6
+@11
+D=A
+@R13
+M=D
+@SP
+AM=M-1
+D=M
+@R13
+A=M
+M=D
+
+// push local 0
+@LCL
+D=M
+@0
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push that 5
+@THAT
+D=M
+@5
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
+
+// push argument 1
+@ARG
+D=M
+@1
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// sub
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+
+// push this 6
+@THIS
+D=M
+@6
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push this 6
+@THIS
+D=M
+@6
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
+
+// sub
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+
+// push temp 6
+@11
+A=A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
--- projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.asm
+++ projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.asm
@@ -1,23 +1,86 @@
-// INIT @SP | write_init
+
 @256
 D=A
 @SP
 M=D
-// push argument 1 | write_push
+
+@RETURN_LABEL0
+D=A
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+@LCL
+D=M
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+@ARG
+D=M
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+@THIS
+D=M
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+@THAT
+D=M
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+@SP
+D=M
+@5
+D=D-A
+@0
+D=D-A
+@ARG
+M=D
+
+@SP
+D=M
+@LCL
+M=D
+
+@Sys.init
+0;JMP
+
+(RETURN_LABEL0)
+
 @ARG
 D=M
 @1
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// pop pointer 1 | write_pop
-@4
+
+@THAT
 D=A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -26,20 +89,19 @@ D=M
 @R13
 A=M
 M=D
-// push constant 0 | write_push
+
 @0
 D=A
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// pop that 0 | write_pop
+
 @THAT
 D=M
 @0
 D=D+A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -48,20 +110,19 @@ D=M
 @R13
 A=M
 M=D
-// push constant 1 | write_push
+
 @1
 D=A
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// pop that 1 | write_pop
+
 @THAT
 D=M
 @1
 D=D+A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -70,36 +131,36 @@ D=M
 @R13
 A=M
 M=D
-// push argument 0 | write_push
+
 @ARG
 D=M
 @0
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// push constant 2 | write_push
+
 @2
 D=A
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
+
 @SP
 AM=M-1
 D=M
 A=A-1
 M=M-D
-// pop argument 0 | write_pop
+
 @ARG
 D=M
 @0
 D=D+A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -108,63 +169,64 @@ D=M
 @R13
 A=M
 M=D
-// write_label
-(FibonacciSeries.$nofunction$MAIN_LOOP_START)
-// push argument 0 | write_push
+
+(MAIN_LOOP_START)
+
 @ARG
 D=M
 @0
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// if-goto FibonacciSeries.$nofunction$COMPUTE_ELEMENT | write_if
+
 @SP
 AM=M-1
 D=M
-@FibonacciSeries.$nofunction$COMPUTE_ELEMENT
+A=A-1
+@COMPUTE_ELEMENT
 D;JNE
-// goto FibonacciSeries.$nofunction$END_PROGRAM | write_goto
-@FibonacciSeries.$nofunction$END_PROGRAM
+
+@END_PROGRAM
 0;JMP
-// write_label
-(FibonacciSeries.$nofunction$COMPUTE_ELEMENT)
-// push that 0 | write_push
+
+(COMPUTE_ELEMENT)
+
 @THAT
 D=M
 @0
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// push that 1 | write_push
+
 @THAT
 D=M
 @1
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
+
 @SP
 AM=M-1
 D=M
 A=A-1
-M=D+M
-// pop that 2 | write_pop
+M=M+D
+
 @THAT
 D=M
 @2
 D=D+A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -173,31 +235,30 @@ D=M
 @R13
 A=M
 M=D
-// push pointer 1 | write_push
-@4
+
+@THAT
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// push constant 1 | write_push
+
 @1
 D=A
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
+
 @SP
 AM=M-1
 D=M
 A=A-1
-M=D+M
-// pop pointer 1 | write_pop
-@4
+M=M+D
+@THAT
 D=A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -206,36 +267,36 @@ D=M
 @R13
 A=M
 M=D
-// push argument 0 | write_push
 @ARG
 D=M
+
 @0
 A=D+A
 D=M
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
-// push constant 1 | write_push
+
 @1
 D=A
-// push D on stack
+@SP
+A=M
+M=D
 @SP
 M=M+1
-A=M-1
-M=D
+
 @SP
 AM=M-1
 D=M
 A=A-1
 M=M-D
-// pop argument 0 | write_pop
+
 @ARG
 D=M
 @0
 D=D+A
-// Write D to R13, pop from stack, write to *R13
 @R13
 M=D
 @SP
@@ -244,12 +305,8 @@ D=M
 @R13
 A=M
 M=D
-// goto FibonacciSeries.$nofunction$MAIN_LOOP_START | write_goto
-@FibonacciSeries.$nofunction$MAIN_LOOP_START
+
+@MAIN_LOOP_START
 0;JMP
-// write_label
-(FibonacciSeries.$nofunction$END_PROGRAM)
-// write_endless_loop
-(FibonacciSeries.$nofunction$genlabel$1)
-@FibonacciSeries.$nofunction$genlabel$1
-0;JMP
+
+(END_PROGRAM)
