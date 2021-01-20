fed4304a689d7e49dc7d167dbf7ab6f158984efa
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-01-20 15:02:59 +0300

    added comments to assemblers/js/index.js

--- tools/Assemblers/js/index.js
+++ tools/Assemblers/js/index.js
@@ -1,31 +1,35 @@
+// Импортируем File Storage "fs" модуль для работы с файловой системой
 const fs = require("fs");
+// Импортируем класс Assembler из файла рядом Assembler.js
 const Assembler = require("./Assembler.js");
-const { performance } = require("perf_hooks");
 
-const [
-    // eslint-disable-next-line no-unused-vars
-    nodeExecPath,
-    // eslint-disable-next-line no-unused-vars
-    thisFilePath,
-    inputFile,
-    outputFile = `${/.*(?=\.asm)/i.exec(inputFile)}.hack`,
-] = process.argv;
+// Получаем аргументы с которыми был запущен скрипт
+const inputFile = process.argv[3];
+const outputFile = process.argv[4];
 
-console.info("args: ", { inputFile, outputFile });
+// Выводим аргументы в консоль, чтобы посмотреть на них в момент запуска скрипта
+console.log("args: ", { inputFile, outputFile });
 
-if (!inputFile.endsWith(".asm"))
-    throw new Error("Only .asm file can be assembled into .hack");
+// Если аргумент inputFile не был передан или это не .asm файл, выбрасываем ошибку
+if (!inputFile || !inputFile.endsWith(".asm"))
+  throw new Error("Only .asm file can be assembled into .hack");
 
+// Если аргумент outputFile не был передан или это не .hack файл, выбрасываем ошибку
+if (!outputFile || !outputFile.endsWith(".hack"))
+  throw new Error("Only .hack file can be served as output");
+
+// Создаем экземпляр класса Assembler
 const assembler = new Assembler();
 
-console.info("Started assembling ⏳");
+// Пишем в консоль о начале выполнения скрипта
+console.log("Started assembling ⏳");
 
-const started = performance.now();
+// Читаем файл по адресу [inputFile], сохраняем содержание файла в переменную assembly
 const assembly = fs.readFileSync(inputFile, "utf8");
+// Компилируем код на языке ассемблера в машинный язык, сохраняем результат в перменную binary
 const binary = assembler.assemble(assembly);
+// Создаем файл по адресу [outputFile], записываем результат компиляции в файл
 fs.writeFileSync(outputFile, binary);
-const finished = performance.now();
 
-console.info(
-    `Finished assembling 🌞 took ${(finished - started).toFixed(2)} ms`
-);
+// Пишем в консоль о завершении выполнения скрипта
+console.log(`Finished assembling 🌞`);
