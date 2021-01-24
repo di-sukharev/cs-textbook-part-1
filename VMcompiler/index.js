9fcef98e4f880b12004c587b7800c193f53ff97d
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-01-24 18:18:59 +0300

    deleted vm compiler

--- VMcompiler/index.js
+++
@@ -1,7 +0,0 @@
-const translateDirectory = require("./translate.js");
-
-const [nodePath, scriptPath, inputDirectoryName, outputFilePath] = process.argv;
-
-translateDirectory(inputDirectoryName);
-
-console.log("Translated!!! Yo");
