42993d200aa72b5c55b7e0ec5a2f74764a042463
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-01 18:05:10 +0300

    finished SymbolTable api

--- tools/Compilers/js/SymbolTable.js
+++ tools/Compilers/js/SymbolTable.js
@@ -1,21 +1,62 @@
 class SymbolTable {
+    classScope = {};
+    subroutineScope = {};
+
     constructor() {
         return this;
     }
 
-    define() {}
+    define({ kind, type, name }) {
+        if (this.classScope[name] || this.subroutineScope[name])
+            throw new Error("Variable already exists:", kind, type, name);
 
-    getVarCount() {}
+        switch (kind) {
+            case "static":
+            case "field":
+                this.classScope[name] = {
+                    kind,
+                    type,
+                    index: Object.keys(this.classScope).length,
+                };
+                break;
+            case "arg":
+            case "var":
+                this.subroutineScope[name] = {
+                    kind,
+                    type,
+                    index: Object.keys(this.subroutineScope).length,
+                };
+                break;
+            default:
+                throw Error("Unknown identifier kind:", kind, type, name);
+        }
+    }
 
-    getTypeOf() {}
+    getVarCount(kind) {
+        const filterByKind = (v) => v.kind === kind;
+        const getCount = (obj) =>
+            Object.values(obj).filter(filterByKind).length;
 
-    getKindOf() {}
+        return getCount(this.classScope) + getCount(this.subroutineScope);
+    }
 
-    getIndexOf() {}
+    getTypeOf(name) {
+        return this.classScope[name]?.type || this.subroutineScope[name]?.type;
+    }
 
-    clearClass() {}
+    getKindOf(name) {
+        return this.classScope[name]?.kind || this.subroutineScope[name]?.kind;
+    }
 
-    clearSubroutine() {}
+    getIndexOf(name) {
+        return (
+            this.classScope[name]?.index || this.subroutineScope[name]?.index
+        );
+    }
+
+    clearSubroutine() {
+        this.subroutineScope = {};
+    }
 }
 
 module.exports = SymbolTable;
