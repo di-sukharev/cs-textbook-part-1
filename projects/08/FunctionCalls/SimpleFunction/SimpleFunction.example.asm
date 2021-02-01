87dd8a736b5275c5e55fd80f09b435b208d6e4ae
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-01 09:24:31 +0300

    added 08 project example files from master

---
+++ projects/08/FunctionCalls/SimpleFunction/SimpleFunction.example.asm
@@ -0,0 +1,125 @@
+// this test needs no initialization of SP and segments
+
+// function SimpleFunction.test 2
+(SimpleFunction.test)
+@2
+D=A
+@SP
+AM=D+M
+A=A-1
+M=0
+A=A-1
+M=0
+
+// push local 0
+@LCL
+D=M
+@0
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// push local 1
+@LCL
+D=M
+@1
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
+
+// not
+@SP
+A=M-1
+M=!M
+
+// push argument 0
+@ARG
+D=M
+@0
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M+D
+
+// push argument 1
+@ARG
+D=M
+@1
+A=D+A
+D=M
+@SP
+M=M+1
+A=M-1
+M=D
+
+// sub
+@SP
+AM=M-1
+D=M
+A=A-1
+M=M-D
+
+// return
+@5
+D=A
+@LCL
+A=M-D
+D=M
+@returnAddress
+M=D
+@SP
+A=M-1
+D=M
+@ARG
+A=M
+M=D
+D=A+1
+@SP
+M=D
+@LCL
+D=M
+@R13
+AM=D-1
+D=M
+@THAT
+M=D
+@R13
+AM=M-1
+D=M
+@THIS
+M=D
+@R13
+AM=M-1
+D=M
+@ARG
+M=D
+@R13
+AM=M-1
+D=M
+@LCL
+M=D
+@returnAddress
+A=M
+0;JMP
