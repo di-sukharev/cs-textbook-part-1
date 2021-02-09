aa6708195a25ab3e634fb2d9a268dc0173965e63
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-02-09 16:56:19 +0300

    fixed tokenizer

--- tools/Compilers/js/Tokenizer.js
+++ tools/Compilers/js/Tokenizer.js
@@ -19,7 +19,7 @@ class Tokenizer {
 
     _tokenize(jackSourceCode) {
         const keywords =
-                "(?<keyword>class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)",
+                "(?<keyword>class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)\\b",
             symbols = "(?<symbol>[{}()\\[\\]\\.;,&\\+\\-\\*\\/|<>=~])",
             identifiers = "(?<identifier>\\w+)",
             integers = "(?<integerConstant>\\d+)",
