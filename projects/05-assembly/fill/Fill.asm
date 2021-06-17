1f48491982cdff08ec099640a9928d1f6f714e46
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-06-17 16:33:41 +0300

    added instructions how2runProgramm

--- projects/05-assembly/fill/Fill.asm
+++ projects/05-assembly/fill/Fill.asm
@@ -1,13 +1,21 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// Программа: Fill.asm
+// В бесконечном цикле слушает ввод с клавиатуры.
+// Когда кнопка нажата (любая), программа закрашивает экран в черный,
+// то есть записывает "черный" в каждый пиксель;
+// экран должен оставаться черным пока клавиша нажата. 
+// Когда клавиша не нажата программа очищает экран,
+// то есть записывает "белый" в каждый пиксель;
+// экран должен оставаться белым пока клавиша не нажата.
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// Перед написанием программы прочитайте инструкцию README.md,
+// она находится в папке с этим проектом.
 
+// Вы также можете изучить программу Rect.asm, запустите ее.
+// Файл Rect.asm находится рядом с этим файлом.
+// Программа запускается в симуляторе процессора,
+// подробнее об этом написано в конце README инструкции.
+
+// При запуске программы не забудьте включить опцию `Animate: no animation`
+// в симуляторе процессора.
+
+// Пишите код ниже:
--- projects/05-assembly/mult/mult.asm
+++ projects/05-assembly/mult/mult.asm
@@ -1,8 +1,7 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+// Программа: Mult.asm 
+// Умножает R0 на R1 и сохраняет результат в R2.
+// (R0, R1, R2 — это RAM[0], RAM[1] и RAM[2], соответственно)
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Перед написанием программы прочитайте инструкцию README.md,
+// она находится в папке с этим проектом.
 
